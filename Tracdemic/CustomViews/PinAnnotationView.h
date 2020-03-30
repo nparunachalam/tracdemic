230e02e078f5fcf37203a90ef9d69c829fbb154e
Arun Packirisamy <nparunachalam@gmail.com>
2020-03-30 00:00:32 -0700

    Record me action added

---
+++ Tracdemic/CustomViews/PinAnnotationView.h
@@ -0,0 +1,17 @@
+//
+//  PinAnnotationView.h
+//  Tracdemic
+//
+//  Created by Oruvan on 3/29/20.
+//  Copyright © 2020 Oruvan. All rights reserved.
+//
+
+#import <MapKit/MapKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PinAnnotationView : MKAnnotationView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Tracdemic/CustomViews/PinAnnotationView.m
@@ -0,0 +1,21 @@
+//
+//  PinAnnotationView.m
+//  Tracdemic
+//
+//  Created by Oruvan on 3/29/20.
+//  Copyright © 2020 Oruvan. All rights reserved.
+//
+
+#import "PinAnnotationView.h"
+
+@implementation PinAnnotationView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
