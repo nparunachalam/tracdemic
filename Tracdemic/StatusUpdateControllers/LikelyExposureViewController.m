a8efe77a214f2b0701331643e5904a05fd55e8e4
Pavan Chayanam <pkchayanam@gmail.com>
2020-03-30 05:04:38 -0700

    Med Dia

--- Tracdemic/StatusUpdateControllers/LikelyExposureViewController.m
+++ Tracdemic/StatusUpdateControllers/LikelyExposureViewController.m
@@ -15,6 +15,15 @@
 @property (strong, nonatomic) UILabel* exposureTypeLabel;
 @property (strong, nonatomic) NSArray* exposureTypeOptionsArray;
 
+@property (strong, nonatomic) UIPickerView* exposureTypePicker;
+@property (strong, nonatomic) UIPickerView* exposureDatePicker;
+@property (strong, nonatomic) UIPickerView* exposureTimePicker;
+
+@property (strong, nonatomic) UITextField* exposureLocationTF;
+@property (strong, nonatomic) UITextField* exposureTypeTF;
+@property (strong, nonatomic) UITextField* exposureDateTF;
+@property (strong, nonatomic) UITextField* exposureTimeTF;
+
 @end
 
 @implementation LikelyExposureViewController
@@ -23,12 +32,20 @@
 @synthesize exposureTypeLabel;
 @synthesize exposureTypeOptionsArray;
 
+@synthesize exposureTypePicker;
+@synthesize exposureDatePicker;
+@synthesize exposureTimePicker;
+@synthesize exposureLocationTF;
+@synthesize exposureTypeTF;
+@synthesize exposureDateTF;
+@synthesize exposureTimeTF;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self.view setBackgroundColor:[UIColor whiteColor]];
     [self setTitle:@"Likely Exposure"];
     
-    exposureTypeOptionsArray=[[NSArray alloc]initWithObjects:@"Dogs", @"Cats", @"Tiger",@"Lion",@"Elephant",nil];
+    exposureTypeOptionsArray=[[NSArray alloc]initWithObjects:@"Direct Contact", @"Airborne", @"Inset Bite", @"Water Borne", @"Articles", @"Fluid Exchange", nil];
     
     tableView = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStylePlain];
 
@@ -43,12 +60,62 @@
     // add to canvas
     [self.view addSubview:tableView];
 
+    exposureLocationTF =[[UITextField alloc] initWithFrame:CGRectMake(5, 5, 300,30)];
+    exposureLocationTF.textColor=[UIColor blackColor];
+    exposureLocationTF.placeholder = @"Location";
 
-    UIPickerView *picker=[[UIPickerView alloc]init];
-    picker.dataSource=self;
-    picker.delegate=self;
-    [picker setShowsSelectionIndicator:YES];
+    exposureTypeTF =[[UITextField alloc] initWithFrame:CGRectMake(5, 5, 300,30)];
+    exposureTypeTF.textColor=[UIColor blackColor];
+    exposureTypeTF.placeholder = @"Exposure Type";
+
+    exposureDateTF =[[UITextField alloc] initWithFrame:CGRectMake(5, 5, 300,30)];
+    exposureDateTF.textColor=[UIColor blackColor];
+    exposureDateTF.placeholder = @"Date";
+
+    exposureTimeTF =[[UITextField alloc] initWithFrame:CGRectMake(5, 5, 300,30)];
+    exposureTimeTF.textColor=[UIColor blackColor];
+    exposureTimeTF.placeholder = @"Time";
+
+    exposureTypePicker =[[UIPickerView alloc]init];
+    exposureTypePicker.dataSource=self;
+    exposureTypePicker.delegate=self;
+    [exposureTypePicker setShowsSelectionIndicator:YES];
+    [exposureTypeTF setInputView:exposureTypePicker];
+
+    exposureTimePicker =[[UIPickerView alloc]init];
+    exposureTimePicker.dataSource=self;
+    exposureTimePicker.delegate=self;
+    [exposureTimePicker setShowsSelectionIndicator:YES];
+    [exposureTimeTF setInputView:exposureTimePicker];
+
+    exposureDatePicker =[[UIPickerView alloc]init];
+    exposureDatePicker.dataSource=self;
+    exposureDatePicker.delegate=self;
+    [exposureDatePicker setShowsSelectionIndicator:YES];
+    [exposureTimeTF setInputView:exposureDatePicker];
+
+    UIToolbar *toolBar=[[UIToolbar alloc]initWithFrame:CGRectMake(0, 0, 320, 44)];
+    [toolBar setTintColor:[UIColor grayColor]];
+    UIBarButtonItem *doneBtn=[[UIBarButtonItem alloc]initWithTitle:@"Done" style:UIBarButtonItemStyleBordered target:self action:@selector(removePicker:)];
+    UIBarButtonItem *space=[[UIBarButtonItem alloc]initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil];
+    [toolBar setItems:[NSArray arrayWithObjects:space,doneBtn, nil]];
+    [exposureTypeTF setInputAccessoryView:toolBar];
+    [exposureTimeTF setInputAccessoryView:toolBar];
+    [exposureDateTF setInputAccessoryView:toolBar];
+
+    UIBarButtonItem* saveButton = [[UIBarButtonItem alloc]
+                                   initWithTitle:@"Save"
+                                   style:UIBarButtonItemStyleBordered
+                                   target:self
+                                   action:@selector(saveSymptomsButtonClicked:)];
+    self.navigationItem.rightBarButtonItem = saveButton;
     
+}
+
+-(void)removePicker:(id)sender {
+    [self.exposureTypeTF resignFirstResponder];
+    [self.exposureTimeTF resignFirstResponder];
+    [self.exposureDateTF resignFirstResponder];
 }
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)theTableView {
@@ -56,7 +123,7 @@
 }
 
 - (NSInteger)tableView:(UITableView *)theTableView numberOfRowsInSection:(NSInteger)section {
-    return 3;
+    return 4;
 }
 
 // the cell will be returned to the tableView
@@ -72,16 +139,33 @@
     }
     
     if(indexPath.row == 0) {
-        [cell.textLabel setText:@"Location"];
+        [cell.contentView addSubview:exposureLocationTF];
     }
     else if(indexPath.row == 1) {
-        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:cellIdentifier];
-        [cell.textLabel setText:@"Exposure Type"];
-        exposureTypeLabel = cell.detailTextLabel;
-        [cell.detailTextLabel setText:@"Airborne"];
+        [cell.contentView addSubview:exposureTypeTF];
+    }
+    else if(indexPath.row == 2) {
+        [cell.contentView addSubview:exposureDateTF];
+        
+        NSDate *today = [NSDate date];
+        NSDateFormatter *dateFormat = [[NSDateFormatter alloc] init];
+        [dateFormat setDateFormat:@"dd/MM/yyyy"];
+        NSString *dateString = [dateFormat stringFromDate:today];
+        
+        exposureDateTF.attributedPlaceholder =
+        [[NSAttributedString alloc] initWithString:[NSString stringWithFormat:@"Date: %@", dateString] attributes:@{ NSForegroundColorAttributeName : [UIColor blackColor] }];
+        
     }
     else {
-        [cell.textLabel setText:@"Approximate Time"];
+        [cell.contentView addSubview:exposureTimeTF];
+
+        NSDate *today = [NSDate date];
+        NSDateFormatter *timeFormat = [[NSDateFormatter alloc] init];
+        [timeFormat setDateFormat:@"HH:mm a Z"];
+        NSString *timeString = [timeFormat stringFromDate:today];
+        
+        exposureTimeTF.attributedPlaceholder =
+        [[NSAttributedString alloc] initWithString:[NSString stringWithFormat:@"Time: %@", timeString] attributes:@{ NSForegroundColorAttributeName : [UIColor blackColor] }];
     }
 
     cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
@@ -124,7 +208,7 @@
 
 - (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component
 {
-    self.exposureTypeLabel.text=[exposureTypeOptionsArray objectAtIndex:row];
+    self.exposureTypeTF.text=[exposureTypeOptionsArray objectAtIndex:row];
 }
 
 @end
